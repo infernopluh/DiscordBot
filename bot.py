3665468adfd4543f0a5e1b46d92e649892e73d90
walkslow <santinobonomolo142@gmail.com>
2025-05-13 10:27:48 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,9 @@ import os
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Load all cogs in the cogs directory
 for filename in os.listdir("./cogs"):
-    if filename.endswith(".py"):
+    if filename.endswith(".py") and not filename.startswith("__"):
         bot.load_extension(f"cogs.{filename[:-3]}")
 
 @bot.event
