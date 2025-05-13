fc5a8213008f158e5fce69404f7df5da760b2a90
walkslow <santinobonomolo142@gmail.com>
2025-05-13 10:24:58 -0400

    Create badwords.py

---
+++ cogs/badwords.py
@@ -0,0 +1,15 @@
+# This file contains a list of very offensive language and slurs for automoderation purposes only.
+# DO NOT print, log, or share this list. Keep in source control only if your repository is private.
+
+BAD_WORDS = [
+    # Common English profanity
+    "fuck", "shit", "bitch", "asshole", "bastard", "dick", "pussy", "cunt", "faggot",
+    "nigger", "nigga", "retard", "slut", "whore", "cock", "douche", "motherfucker",
+    "twat", "wanker", "prick", "chink", "gook", "spic", "kike", "dyke", "tranny",
+    "coon", "cameltoe", "fag", "cum", "jizz", "rape", "rapist", "molest", "pedophile",
+    # Variants with leetspeak or common misspellings (add as needed)
+    "fuk", "sh1t", "b1tch", "f4ggot", "n1gger", "n1gga", "r3tard", "slvt", "wh0re",
+    "c0ck", "d1ck", "tw4t", "w4nker", "pr1ck", "ch1nk", "g00k", "sp1c", "k1ke",
+    "cumslut", "cumdump", "rap3", "p3dophile", "pedo", "molestor",
+    
+]
